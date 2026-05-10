62886a980e5817a07736b4d9e2e483247a43746d
tanyongkiam <tanyongkiam@gmail.com>
2026-05-10 23:37:26 +0800

    update submodule commit hashes

--- demo/run.sh
+++ demo/run.sh
@@ -3,7 +3,7 @@ set -ex
 
 tar -xzvf demo.tgz
 
-../impcake/build/impcheck_check -fifo-directives=directives.0.impcheck -fifo-feedback=out.impcheck -key-seed=13805254743912277295
-../impcake/build/impcheck_check -fifo-directives=directives.1.impcheck -fifo-feedback=out.impcheck -key-seed=13805254743912277295
-../impcake/build/impcheck_check -fifo-directives=directives.2.impcheck -fifo-feedback=out.impcheck -key-seed=13805254743912277295
-../impcake/build/impcheck_check -fifo-directives=directives.3.impcheck -fifo-feedback=out.impcheck -key-seed=13805254743912277295
+../impcake/build/impcheck_check -directives=directives.0.impcheck -feedback=out.impcheck -key-seed=13805254743912277295
+../impcake/build/impcheck_check -directives=directives.1.impcheck -feedback=out.impcheck -key-seed=13805254743912277295
+../impcake/build/impcheck_check -directives=directives.2.impcheck -feedback=out.impcheck -key-seed=13805254743912277295
+../impcake/build/impcheck_check -directives=directives.3.impcheck -feedback=out.impcheck -key-seed=13805254743912277295
